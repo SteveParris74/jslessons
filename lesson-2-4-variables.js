af33cfdde9713560f9ed15ac5f2af20c2bc5d85b
SteveParris74 <steveqube@gmail.com>
2019-01-25 10:34:41 -0800

    second

---
+++ lesson-2-4-variables.js
@@ -0,0 +1,13 @@
+let x = 1
+let y = 2
+let z = 4
+let t = 6
+
+if (x + y === 8) {
+  console.log ('yes')
+} else if (z + t === 3) {
+  console.log ('very much so')
+} else if (y + t === 8) {
+  console.log ('quite!')
+  }
+  // Answer is quite!
